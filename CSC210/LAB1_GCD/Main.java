82419ab242639bedf60e19d5c25147eef2baaf8a
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2020-08-21 19:38:12 +0700

    [ADD] GCD Lab

---
+++ CSC210/LAB1_GCD/Main.java
@@ -0,0 +1,75 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(bruteForceGCD(60, 30));
+        System.out.println(highSchoolGCD(60, 30));
+        System.out.println(commonFactorGCD(64, 32));
+    }
+
+    public static int bruteForceGCD(int m, int n) {
+        int answer = 2;
+        for (int x = 2; x <= Math.min(m, n); x++) {
+            if (m % x == 0 && n % x == 0) {
+                answer = x;
+            }
+        }
+        return answer;
+    }
+
+    public static int highSchoolGCD(int m, int n) {
+        int answer = 1;
+        for (int i = 2; i <= Math.min(m, n); i++) {
+            while (m % i == 0 && n % i == 0) {
+                m /= i;
+                n /= i;
+                answer = answer * i;
+            }
+        }
+        return answer;
+    }
+
+    public static int commonFactorGCD(int m, int n) {
+        int answer = 1;
+        // Find lists of factors of m and n
+        int maxFactor = (int) (Math.log(m) / Math.log(2));
+        int[] factors4m = new int[maxFactor + 1];
+        factors(m, factors4m);
+
+        maxFactor = (int) (Math.log(n) / Math.log(2));
+        int[] factors4n = new int[maxFactor + 1];
+        factors(n, factors4n);
+
+        int pointM = 0, pointN = 0; // Pointer for running the factors
+        while (factors4m[pointM] != -1 && factors4n[pointN] != -1 && pointM < factors4m.length
+                && pointN < factors4n.length) {
+            if (factors4m[pointM] == factors4n[pointN]) {
+                answer = answer * factors4m[pointM];
+                pointM++;
+                pointN++;
+            } else if (factors4m[pointM] < factors4n[pointN]) {
+                pointM++;
+            } else {
+                pointN++;
+            }
+        }
+        return answer;
+    }
+
+    public static void factors(int x, int[] factors) {
+        int i = 0, factor = 2;
+        while (factor <= x) {
+            while (x % factor == 0) {
+                x = x / factor;
+                factors[i] = factor;
+                i++;
+            }
+            factor++;
+        }
+        if (i < factors.length) {
+            factors[i] = -1;
+        }
+    }
+}
