2d97aafa84e2b54d7f0cee30507bf7886555fa55
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2020-08-28 18:54:12 +0700

    [ADD] Complete Horizontal L2R, R2L

--- CSC210/LAB2_Puzzle/Main.java
+++ CSC210/LAB2_Puzzle/Main.java
@@ -7,34 +7,41 @@ public class Main {
         char[][] puzzle = {
             {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
             {'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'},
-            {'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'},
-            {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
+            {'e', 'f', 'r', 'o', 'p', 'o', 'k', 'l'},
+            {'a', 'b', 'c', 'd', 'e', 'c', 'a', 'n'},
             {'a', 'p', 'e', 'e', 'm', 'l', 'g', 'h'},
-            {'p', 'q', 'r', 's', 'm', 'a', 'r', 't'},
+            {'d', 'n', 'a', 's', 'm', 'a', 'r', 't'},
             {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
             {'m', 'n', 'o', 'p', 'q', 'r', 's', 't'},
         };
 
         int[] position = new int[2];
-        position = search(puzzle, "smart");
+        position = search(puzzle, "sand");
         for(int i=0;i<2;i++){
             System.out.print(position[i] + " ");
         }
-        System.out.println("");
-        // position = search(puzzle, "hello");
-        // for(int i=0;i<2;i++){
-        //     System.out.print(position[i] + " ");
-        // }
     }
 
     public static int[] search(char[][] a, String key){
         int[] location = { -1, -1 };
         for(int row=0; row < a.length; row++){
-            for(int i=0; i <= a[0].length - key.length(); i++){
+            for(int i=0; i < a[0].length; i++){
                 // Horizontal Search
                 // i point for each colume
+                // Left to right
                 int j = 0; //j is pointer for key
-                while(j<key.length() && a[row][i+j] == key.charAt(j)){
+                while(i<=a[0].length - key.length() 
+                && j < key.length() && a[row][i+j] == key.charAt(j)){
+                    j++;
+                }
+                if(j == key.length()){
+                    location[0] = row;
+                    location[1] = i;
+                    return location;
+                }
+                // Right to left
+                j = 0; //j is pointer for key
+                while(i>=key.length()-1 && j<key.length() && a[row][i-j] == key.charAt(j)){
                     j++;
                 }
                 if(j == key.length()){
