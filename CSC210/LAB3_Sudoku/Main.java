022aa12937de7bacd5c787d5003d9196a7ca3065
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2020-09-18 19:07:49 +0700

    Finish Sudoku

--- CSC210/LAB3_Sudoku/Main.java
+++ CSC210/LAB3_Sudoku/Main.java
@@ -2,7 +2,10 @@
  * Main
  */
 public class Main {
-    static final int ROW = 8, COLUMN = 8, CELL = 81;
+    // Sethanant Pipatpakorn 
+    //62130500230 18 Sep 2020
+    
+    static final int ROW = 9, COLUMN = 9, CELL = 81;
     public static void main(String[] args) {
         int[][] board = {
             {7,0,0,5,8,3,0,0,6},
@@ -15,10 +18,43 @@ public class Main {
             {0,0,3,1,5,0,0,7,2},
             {2,1,5,6,0,0,0,3,0}
         };
-        System.out.println(isValid(board, 6, 0, 7));
-        System.out.println(isValid(board, 1, 6, 2));
-        System.out.println(isValid(board, 7, 6, 2));
+        // System.out.println(isValid(board, 6, 0, 7));
+        // System.out.println(isValid(board, 1, 6, 2));
+        // System.out.println(isValid(board, 7, 6, 2));
+        // // isValid but not F O R S U R E
+        // System.out.println(isValidForSure(board, 7, 3, 2));
+        // System.out.println(isValid(board, 7, 3, 2));
 
+        playSudoku(board);
+        printBoard(board);
+    }
+
+    public static void playSudoku(int[][] a){
+        int nonZero = countNonZero(a);
+        while(nonZero < CELL) {
+            for(int i=0; i<ROW; i++){
+                for(int j=0; j<COLUMN; j++){
+                    if(a[i][j] == 0){
+                        for(int value=1; value <=9; value++){
+                            if(isValidForSure(a, value, i, j)){
+                                a[i][j] = value;
+                                nonZero++;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void printBoard(int[][] a){
+        for(int i=0; i<ROW; i++){
+            for(int j=0; j<COLUMN; j++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 
     public static boolean isValid(int[][] a, int value, int rowX, int columnY) {
@@ -39,5 +75,34 @@ public class Main {
             }
         }
         return true;
+    }
+
+    // Only one possible way to put it?
+    public static boolean isValidForSure(int[][] a, int value, int rowX, int columnY){
+        if(isValid(a, value, rowX, columnY)){
+            int possible = 0;
+            for(int i=0; i<3; i++){
+                for(int j=0; j<3;j++){
+                    if(a[(rowX/3)*3+i][(columnY/3)*3+j] == 0 &&
+                    isValid(a, value, (rowX/3)*3+i, (columnY/3)*3+j)){
+                        possible++;
+                    }
+                }
+            }
+            if(possible == 1) return true;
+        }
+        return false;
+    }
+
+    public static int countNonZero(int[][] a){
+        int count = 0;
+        for(int i=0;i<ROW;i++){
+            for(int j=0;j<COLUMN;j++){
+                if(a[i][j] != 0){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
