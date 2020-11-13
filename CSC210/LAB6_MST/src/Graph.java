c47fd2617a66362b7eea764015c5a59a40dcf9ab
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-11-13 19:31:27 +0700

    [FINISH] LAB6

--- CSC210/LAB6_MST/src/Graph.java
+++ CSC210/LAB6_MST/src/Graph.java
@@ -46,5 +46,47 @@ public class Graph {
             Edge x = iterator.next();
             System.out.println(x.to + "(" + x.from + "," + x.distance + ")");
         }
+        System.out.println();
+        Edge minEdge = remainingVertices.removeFirst();
+        treeVertices.add(minEdge);
+
+        ListIterator<Edge> iteratorForRemain;
+        Edge eRemain;
+        while (!remainingVertices.isEmpty()) {
+            min = Integer.MAX_VALUE;
+            int minIndex = 0;
+            iteratorForRemain = remainingVertices.listIterator();
+            // Check if which remaining vertices that has distance more than new node in treeVertices
+            for (int i=0; iteratorForRemain.hasNext(); i++){
+                eRemain = iteratorForRemain.next();
+                iterator = nodeList[minEdge.to].listIterator();
+                while(iterator.hasNext()){
+                    e = iterator.next();
+                    if(e.to == eRemain.to && e.distance < eRemain.distance){
+                        eRemain.from = e.from;
+                        eRemain.distance = e.distance;
+                    }
+                }
+                if(eRemain.distance < min){
+                    minIndex = i;
+                    min = eRemain.distance;
+                }
+            }
+            iterator = remainingVertices.listIterator();
+            while (iterator.hasNext()){
+                Edge x = iterator.next();
+                System.out.print(x.to + "(" + x.from + "," + x.distance + "), ");
+            }
+            System.out.println();
+            minEdge = remainingVertices.remove(minIndex);
+            treeVertices.addFirst(minEdge);
+        }
+
+        System.out.println("\nMinimum Spanning Tree compose of the following edges\n");
+        iterator = treeVertices.listIterator();
+        while (iterator.hasNext()){
+            Edge x = iterator.next();
+            System.out.print(x.to + "(" + x.from + "," + x.distance + ") ");
+        }
     }
 }
