aee07c384c00af9aad5a325cfb3747097616c42f
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-02-18 23:26:45 +0700

    finish

--- CSC234/form_assignment/lib/main.dart
+++ CSC234/form_assignment/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// Sethanant Pipatpakorn 62130500230
 void main() {
   runApp(MyApp());
 }
@@ -9,109 +10,118 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Form Assignment',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.cyan,
+        accentColor: Colors.amber.shade200,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _studentIdFocusNode = FocusNode();
+  final _nameFocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  var _name = '';
+  var _studentId = '';
+
+  void _saveForm() {
+    final isValid = this._form.currentState.validate();
+    if (!isValid) {
+      return;
+    }
+    this._form.currentState.save();
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Your information'),
+        content: Container(
+          height: 100,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Name: $_name'),
+              SizedBox(height: 10),
+              Text('Student ID: $_studentId'),
+            ],
+          ),
+        ),
+        actions: [
+          FlatButton(
+            child: Text('Okay'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _studentIdFocusNode.dispose();
+    _nameFocusNode.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Form Fields'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Name'),
+                textInputAction: TextInputAction.next,
+                focusNode: _nameFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_studentIdFocusNode);
+                },
+                validator: (value) =>
+                    value.isEmpty ? 'Please enter name' : null,
+                onSaved: (value) => this._name = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Student ID'),
+                textInputAction: TextInputAction.done,
+                keyboardType: TextInputType.number,
+                focusNode: _studentIdFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_nameFocusNode);
+                },
+                validator: (value) {
+                  if (value.isEmpty) return 'Please enter your student ID';
+                  if (value.length != 11)
+                    return 'Student ID should be 11 digits';
+                  return null;
+                },
+                onSaved: (value) => this._studentId = value,
+              ),
+              SizedBox(height: 50),
+              RaisedButton(
+                child: Text('Done'),
+                color: Theme.of(context).accentColor,
+                onPressed: _saveForm,
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
