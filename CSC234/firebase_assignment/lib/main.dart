e6a96684f71b61825c6090760da6e21200be997a
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-03-05 22:10:48 +0700

    [FINISH] adding secondScreen

--- CSC234/firebase_assignment/lib/main.dart
+++ CSC234/firebase_assignment/lib/main.dart
@@ -9,6 +9,7 @@ import 'package:provider/provider.dart';
 import 'src/authentication.dart';
 import 'src/widgets.dart';
 
+// Sethanant Pipatpakorn 62130500230
 void main() {
   runApp(ChangeNotifierProvider(
     create: (context) => ApplicationState(),
@@ -41,41 +42,46 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isLoggedIn = Provider.of<ApplicationState>(context).loginState ==
+        ApplicationLoginState.loggedIn;
     return Scaffold(
       appBar: AppBar(
         title: Text('Firebase Meetup'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Image.asset('assets/codelab.png'),
-          SizedBox(height: 8),
-          IconAndDetail(Icons.calendar_today, 'October 30'),
-          IconAndDetail(Icons.location_city, 'San Francisco'),
-          Consumer<ApplicationState>(
-            builder: (context, appState, _) => Authentication(
-              email: appState.email,
-              loginState: appState.loginState,
-              startLoginFlow: appState.startLoginFlow,
-              verifyEmail: appState.verifyEmail,
-              signInWithEmailAndPassword: appState.signInWithEmailAndPassword,
-              cancelRegistration: appState.cancelRegistration,
-              registerAccount: appState.registerAccount,
-              signOut: appState.signOut,
+      body: isLoggedIn
+          ? SecondScreen()
+          : ListView(
+              children: <Widget>[
+                Image.asset('assets/codelab.png'),
+                SizedBox(height: 8),
+                IconAndDetail(Icons.calendar_today, 'October 30'),
+                IconAndDetail(Icons.location_city, 'San Francisco'),
+                Consumer<ApplicationState>(
+                  builder: (context, appState, _) => Authentication(
+                    email: appState.email,
+                    loginState: appState.loginState,
+                    startLoginFlow: appState.startLoginFlow,
+                    verifyEmail: appState.verifyEmail,
+                    signInWithEmailAndPassword:
+                        appState.signInWithEmailAndPassword,
+                    cancelRegistration: appState.cancelRegistration,
+                    registerAccount: appState.registerAccount,
+                    signOut: appState.signOut,
+                  ),
+                ),
+                Divider(
+                  height: 8,
+                  thickness: 1,
+                  indent: 8,
+                  endIndent: 8,
+                  color: Colors.grey,
+                ),
+                Header("What we'll be doing"),
+                Paragraph(
+                  'Join us for a day full of Firebase Workshops and Pizza!',
+                ),
+              ],
             ),
-          ),
-          Divider(
-            height: 8,
-            thickness: 1,
-            indent: 8,
-            endIndent: 8,
-            color: Colors.grey,
-          ),
-          Header("What we'll be doing"),
-          Paragraph(
-            'Join us for a day full of Firebase Workshops and Pizza!',
-          ),
-        ],
-      ),
     );
   }
 }
@@ -386,28 +392,35 @@ class YesNoSelection extends StatelessWidget {
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Firebase Meetup')),
-      body: Container(
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.all(10),
         child: Consumer<ApplicationState>(
           builder: (context, appState, _) => Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Add from here
               if (appState.attendees >= 2)
                 Paragraph('${appState.attendees} people going')
               else if (appState.attendees == 1)
                 Paragraph('1 person going')
               else
                 Paragraph('No one going'),
-              // To here.
-              if (appState.loginState == ApplicationLoginState.loggedIn) ...[
-                // Add from here
+              ...[
                 YesNoSelection(
                   state: appState.attending,
                   onSelection: (attending) => appState.attending = attending,
                 ),
-                // To here.
+                Authentication(
+                  email: appState.email,
+                  loginState: appState.loginState,
+                  startLoginFlow: appState.startLoginFlow,
+                  verifyEmail: appState.verifyEmail,
+                  signInWithEmailAndPassword:
+                      appState.signInWithEmailAndPassword,
+                  cancelRegistration: appState.cancelRegistration,
+                  registerAccount: appState.registerAccount,
+                  signOut: appState.signOut,
+                ),
                 Header('Discussion'),
                 GuestBook(
                   addMessage: (String message) =>
