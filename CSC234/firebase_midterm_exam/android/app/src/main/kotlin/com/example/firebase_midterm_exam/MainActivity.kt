08ee7c221d6ebda981628655877dd8e118f8cd9d
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-03-10 23:00:39 +0700

    [INIT]

---
+++ CSC234/firebase_midterm_exam/android/app/src/main/kotlin/com/example/firebase_midterm_exam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_midterm_exam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ CSC234/firebase_midterm_exam/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Placeholder',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Placeholder'),
+      ),
+      body: Center(
+        child: Text('Nothing here'),
+      ),
+    );
+  }
+}
