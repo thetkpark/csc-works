c81b736cda49e2590d67ebbe952d1f3bd0a38613
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-03-11 14:25:18 +0700

    [FINISH] 1

--- CSC234/firebase_midterm_exam/lib/main.dart
+++ CSC234/firebase_midterm_exam/lib/main.dart
@@ -1,26 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
+// Sethanant Pipatpakorn 62130500230
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Placeholder',
-      home: HomeScreen(),
+      title: 'CSC234 Midterm App',
+      theme: ThemeData(primaryColor: Colors.teal),
+      home: HomePage(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _form = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Placeholder'),
+        title: Text('Sign-in form'),
       ),
-      body: Center(
-        child: Text('Nothing here'),
+      body: Form(
+        key: _form,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                textInputAction: TextInputAction.next,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                textInputAction: TextInputAction.done,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Sign in',
+                    style: TextStyle(color: Theme.of(context).primaryColor),
+                  ))
+            ],
+          ),
+        ),
       ),
     );
   }
