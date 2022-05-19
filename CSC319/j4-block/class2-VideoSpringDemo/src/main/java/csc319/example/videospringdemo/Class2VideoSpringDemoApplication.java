45321fd54b72adbb5e59284531fc2279b9f6a67b
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-05-19 11:33:48 +0700

    add jpa

--- CSC319/j4-block/class2-VideoSpringDemo/src/main/java/csc319/example/videospringdemo/Class2VideoSpringDemoApplication.java
+++ CSC319/j4-block/class2-VideoSpringDemo/src/main/java/csc319/example/videospringdemo/Class2VideoSpringDemoApplication.java
@@ -5,8 +5,10 @@ import csc319.example.videospringdemo.repository.VideoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = VideoRepository.class)
 public class Class2VideoSpringDemoApplication {
 
     public static void main(String[] args) {
--- CSC319/j4-block/class2-VideoSpringDemo/src/main/java/csc319/example/videospringdemo/controller/Video.java
+++ CSC319/j4-block/class2-VideoSpringDemo/src/main/java/csc319/example/videospringdemo/controller/Video.java
@@ -2,12 +2,21 @@ package csc319.example.videospringdemo.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter @AllArgsConstructor
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+@Entity
 public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     private String name;
     private String genre;
     private int duration;
-
 }
